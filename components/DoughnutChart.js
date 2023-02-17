11dccc0154a1800d4bda576dec47e905289c75bf
soheib kiani <mrsoheibkiani@gmail.com>
2023-02-17 14:19:04 +0330

    Add Charts

---
+++ components/DoughnutChart.js
@@ -0,0 +1,9 @@
+
+import { Doughnut } from "react-chartjs-2";
+
+
+function DoughnutChart({ chartData }) {
+  return <Doughnut data={chartData} />;
+}
+
+export default DoughnutChart;
--- pages/card/index.js
+++ pages/card/index.js
@@ -1,13 +1,46 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import BarChart from '@/components/BarChart';
+import LineChart from '@/components/LineChart';
+import PieChart from '@/components/PieChart';
+import DoughnutChart from '@/components/DoughnutChart';
+import { UserData } from '@/data/Data';
+import React, { useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+
 const index = () => {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: 'Users Gained',
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          '#ecf0f1',
+          '#50AF95',
+          '#f3ba2f',
+          '#2a71d0',
+        ],
+        borderColor: 'white',
+        borderWidth: 1,
+      },
+    ],
+  });
   return (
     <section>
       <div className="grid grid-cols-4 place-items-center gap-x-2">
-        <div className="w-80 h-52 rounded-lg bg-black bg-opacity-20   drop-shadow-lg ">
-            
+        <div className="w-80 grid place-items-center h-52 rounded-lg  bg-black ">
+          <BarChart chartData={userData} />
         </div>
-        <div className="w-80 h-52 rounded-lg bg-black"></div>
-        <div className="w-80 h-52 rounded-lg bg-black"></div>
-        <div className="w-80 h-52 rounded-lg bg-black"></div>
+        <div className="w-80 grid place-items-center h-52 rounded-lg bg-black">
+          <DoughnutChart chartData={userData} />
+        </div>
+        <div className="w-80 grid place-items-center h-52 rounded-lg bg-black">
+          <PieChart chartData={userData} />
+        </div>
+        <div className="w-80 grid place-items-center h-52 rounded-lg bg-black">
+          <LineChart chartData={userData} />
+        </div>
       </div>
     </section>
   );
