d106825d7ff073d9578d11b3f674b6db19b07aa2
soheib kiani <mrsoheibkiani@gmail.com>
2023-02-17 09:50:00 +0330

    P-1 design od SideBar

--- components/sidebar.js
+++ components/sidebar.js
@@ -4,10 +4,61 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 /* eslint-disable @next/next/no-img-element */
-const Sidebar = ({ toggle }) => {
+const Sidebar = () => {
   return (
-    <div className="w-1/5 bg-red-500 h-screen">
-
+    <div className="w-1/6 bg-blue-700 h-screen">
+      <div className="grid h-full place-items-center">
+        {/* Top Of SideBar */}
+        <section className="flex flex-col justify-center h-1/6 items-center">
+          <div className="h-24 w-24 rounded-full">
+            <img src="/icon/user.png" />
+          </div>
+          <div className="flex items-center mt-5">
+            <div className="text-center font-bold">IOTEAM</div>
+          </div>
+        </section>
+        {/* SideBar Buttons */}
+        <section className="flex h-full ">
+          <div className="flex flex-col gap-y-10 mt-5">
+            <div className="flex items-center text-center gap-x-4">
+              <div className="w-3 h-3">
+                <img src="/icon/home.png" />
+              </div>
+              <div className="font-semibold text-sm">Dashboard</div>
+            </div>
+            <div className="flex items-center text-center gap-x-4">
+              <div className="w-3 h-3">
+                <img src="/icon/home.png" />
+              </div>
+              <div className="font-semibold text-sm">Email</div>
+            </div>
+            <div className="flex items-center text-center gap-x-4">
+              <div className="w-3 h-3">
+                <img src="/icon/home.png" />
+              </div>
+              <div className="font-semibold text-sm">Bar</div>
+            </div>
+            <div className="flex items-center text-center gap-x-4">
+              <div className="w-3 h-3">
+                <img src="/icon/home.png" />
+              </div>
+              <div className="font-semibold text-sm">Pie</div>
+            </div>
+            <div className="flex items-center text-center gap-x-4">
+              <div className="w-3 h-3">
+                <img src="/icon/home.png" />
+              </div>
+              <div className="font-semibold text-sm">Geo</div>
+            </div>
+            <div className="flex items-center text-center gap-x-4">
+              <div className="w-3 h-3">
+                <img src="/icon/home.png" />
+              </div>
+              <div className="font-semibold text-sm">live</div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
